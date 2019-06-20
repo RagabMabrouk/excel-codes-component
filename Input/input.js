3b2e1a41eb39ea4ef0e54a71d8a8590f692acbb4
r-mabrouk <r.mabrouk@billettinc.com>
2019-06-20 12:48:05 +0200

    first push

---
+++ Input/input.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+
+class Input extends Component {
+    constructor(props) {
+        super(props)
+    }
+    render() {
+        return (<View style={{
+            width: 100,
+            height: 100,
+            backgroundColor: 'red'
+        }}>
+
+        </View>)
+    }
+}
+
+export { Input }
